7401b77e3ddc55d87cc7edc91bfcd096fb0fd6bd
hosseinkhatiri <hkhk.sharif@gmail.com>
2023-08-07 13:02:39 -0400

    wip

--- cf_recommender/cf_recommender.py
+++ cf_recommender/cf_recommender.py
@@ -1,34 +1,47 @@
 import pandas as pd
+from surprise import SVD
 from surprise import Reader, Dataset, KNNBasic
 from surprise.model_selection import cross_validate
 
-# Load the data from your csv file
-df = pd.read_csv('dataset/adj_matrix.csv')
-df = df[:50651]
-# Preprocess the dataframe
-# Flatten the dataframe and create interaction matrix
-data = []
-for i, row in df.iterrows():
-    for j in range(1, 51):
-        if row[j] != -1:
-            data.append([row[0], row[j], 1])  # if an interaction exists, rate it as 1
+################# Implicit Feedback CF #################
+'''
+    We just fed user-contract interactions as data to our SVD CF model
+'''
+def data_loader(adj_matrix_path, format):
+    df = pd.read_csv(adj_matrix_path + format) if format == '.csv' else pd.read_parquet(adj_matrix_path + format)
+    df = df[:1]
+    data = []
 
-# Convert list into dataframe
-data = pd.DataFrame(data, columns=['user', 'item', 'rating'])
+    if adj_matrix_path == 'dataset/adj_matrix_kmean_tags':
+        for i, row in df.iterrows():
+            for j in range(1, 51):
+                if row[j] != -1:
+                    data.append([row[0], row[j], 1])
+    
+    if adj_matrix_path == 'dataset/adj_matrix_contract_names':
+        for i, row in df.iterrows():
+            for j in range(1, len(df.columns)):
+                if row[j] != 0:
+                    data.append([row.name, df.columns[j], 1])
 
-# Define the rating scale
-reader = Reader(rating_scale=(0, 1))  # scale is 0 to 1, where 1 is an interaction
+    data = pd.DataFrame(data, columns=['user', 'item', 'rating'])
+    return data
 
-# Load the data into Surprise dataset
+
+# data = data_loader('dataset/adj_matrix_kmean_tags', '.csv')
+data = data_loader('dataset/adj_matrix_contract_names', '.parquet')
+reader = Reader(rating_scale=(0, 1))
 data = Dataset.load_from_df(data[['user', 'item', 'rating']], reader)
 
-# Use user-based collaborative filtering
-sim_options = {
-    'name': 'cosine',
-    'user_based': True  # compute similarities between users
-}
-
-algo = KNNBasic(sim_options=sim_options)
-
-# Run 5-fold cross-validation and print results
+################# SVD CF #####################
+algo = SVD()
 cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+################# KNN CF #####################
+# sim_options = {
+#     'name': 'cosine',
+#     'user_based': True
+# }
+# algo = KNNBasic(sim_options=sim_options)
+# cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
